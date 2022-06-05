7995de41b2ff62876e7f5571e82dd787d40147c2
Dilawar Singh <dilawar@subcom.tech>
2022-06-05 13:12:38 +0530

    fix: Added info module.

--- bmo/doctor.py
+++ bmo/doctor.py
@@ -13,32 +13,11 @@ import typer
 
 app = typer.Typer()
 
-def check_os() -> str:
-    """Return the current operating system."""
-    import platform
-
-    return platform.system()
-
-def get_like_distro():
-    """Get the Linux distro.
-
-    Note: Requires Python3.10
-    """
-    info = platform.freedesktop_os_release()
-    ids = [info["ID"]]
-    if "ID_LIKE" in info:
-        # ids are space separated and ordered by precedence
-        ids.extend(info["ID_LIKE"].split())
-    return ids
-
 
 @app.command()
 def doctor() -> str:
-    res: T.Dict[str, str] = {}
-    res["OS"] = which_os()
-    assert res["OS"] in ["Windows", "Linux", "Darwin"], f"{res['OS']} is not supported"
-    res["Package Manager"] = which_package_manager(res["OS"])
-    return json.dumps(res)
+    logger.warning('Not implemented')
+    return ''
 
 
 if __name__ == "__main__":
--- bmo/info.py
+++ bmo/info.py
@@ -5,6 +5,7 @@ __email__ = "dilawar@subcom.tech"
 # Execute `bmo info` to print system information
 
 import json
+import platform
 import typing as T
 
 from loguru import logger
@@ -21,17 +22,17 @@ def which_os() -> str:
     return platform.system()
 
 
-def get_like_distro():
-    """Get the Linux distro.
-
-    Note: Requires Python3.10
-    """
-    info = platform.freedesktop_os_release()
-    ids = [info["ID"]]
-    if "ID_LIKE" in info:
-        # ids are space separated and ordered by precedence
-        ids.extend(info["ID_LIKE"].split())
-    return ids
+def get_like_distro() -> T.List[str]:
+    """Get the Linux distro."""
+    if int(sys.version_info[1]) >= 10:
+        # FIXME: Requires Python3.10
+        info = platform.freedesktop_os_release()  # type: ignore # pylint: disable=no-member
+        ids = [info["ID"]]
+        if "ID_LIKE" in info:
+            # ids are space separated and ordered by precedence
+            ids.extend(info["ID_LIKE"].split())
+        return ids
+    return []
 
 
 def which_package_manager(system: str) -> T.Optional[str]:
@@ -44,7 +45,7 @@ def info() -> str:
     res: T.Dict[str, str] = {}
     res["OS"] = which_os()
     assert res["OS"] in ["Windows", "Linux", "Darwin"], f"{res['OS']} is not supported"
-    res["Package Manager"] = which_package_manager(res["OS"])
+    res["Package Manager"] = str(which_package_manager(res["OS"]))
     return json.dumps(res)
 
 
