1a4b6a5fd01cc93849774c71752d034945bcc466
Dilawar Singh <dilawar@subcom.tech>
2022-06-09 20:03:42 +0530

    feat: version bump.

--- bmo/run.py
+++ bmo/run.py
@@ -15,36 +15,61 @@ import typer
 
 app = typer.Typer()
 
+
 def determine_linter(dir: Path) -> str:
-    """Find a suitable linter in the current directory.
-    """
-    if (dir / 'pyproject.toml') or (dir / "setup.py"):
+    """Find a suitable linter in the current directory."""
+    if (dir / "pyproject.toml") or (dir / "setup.py"):
         return "mypy"
     return ""
+
+
+def run_command(cmd: str, cwd: Path = Path.cwd(), silent: bool = False) -> int:
+    """Run a given command"""
+    logger.info(f"Running `{cmd}` in {cwd}")
+    proc = subprocess.Popen(cmd.split(), stdout=subprocess.PIPE, cwd=cwd)
+    assert proc is not None
+    for line in io.TextIOWrapper(proc.stdout, encoding="utf8"):  # type: ignore
+        typer.echo(f"> {line}")
+    return 0
+
 
 @app.command()
 def mypy(dir: Path = Path("src")):
     """Run mypy linter in given directory"""
     logger.info(f"Running mypy in {dir}")
     assert dir.exists(), "f{dir} doesn't exists"
-    proc = subprocess.Popen(
-        [
-            sys.executable,
-            "-m",
-            "mypy",
-            "--ignore-missing-imports",
-            "--install-types",
-            "--non-interactive",
-            str(dir),
-        ],
-        stdout=subprocess.PIPE,
+    assert 0 == run_command(
+        f"{sys.executable} -m mypy --ignore-missing-imports --install-types --non-interactive {str(dir)}"
     )
-    assert proc is not None
-    for line in io.TextIOWrapper(proc.stdout, encoding="utf8"): # type: ignore
-        typer.echo(f"> {line}")
+
+
+@app.command("gi")
+def generate_gitignore(args: T.List[str], force: bool = False):
+    """Create gitignore file"""
+    import requests
+
+    cwd = Path.cwd().resolve()
+    if not (cwd / ".git").exists():
+        logger.warning(f"{cwd} is not a git repository?")
+
+    gitignorefile = cwd / ".gitignore"
+    logger.info(f"Getting gitignore for {args}")
+
+    url = "https://www.toptal.com/developers/gitignore/api/"
+    endpoint = ",".join(args)
+    res = requests.get(f"{url}/{endpoint}")
+    if not gitignorefile.exists() or force:
+        with gitignorefile.open("w") as f:
+            f.write(res.text)
+        return
+    logger.warning(
+        f"{gitignorefile} exists. Use `--force` to overwrite. I am going to write to stdout"
+    )
+    typer.echo(res.text)
+
 
 @app.command()
-def lint(linter: str="", dir:T.Optional[Path] = None):
+def lint(linter: str = "", dir: T.Optional[Path] = None):
     """Run a linter. If one is not given, pick one."""
     if dir is None:
         dir = Path.cwd()
