3def4b91185975b3e0a29e3d9a6734d4def1e1d0
Dilawar <dilawar@subcom.tech>
2022-06-09 22:11:58 +0530

    fix: Fixed linter issue and added runner command.

--- bmo/cicd.py
+++ bmo/cicd.py
@@ -23,15 +23,15 @@ def find_docker():
     return shutils.which("docker")
 
 @app.command("runner")
-def run_gitlab_runner( : str = ""):
+def run_gitlab_runner(command: str = "", job: str = "build"):
     """Run gitlab-runner """
     cwd = Path.cwd()
     gitlab_pipeline = cwd / '.gitlab-ci.yml'
     assert gitlab_pipeline.exists(), f"{gitlab_pipeline} doesn't exists"
-    if not runner_cmd:
-        runner_cmd = "docker" if find_docker() is not None else "shell"
-    action = "build"
-    assert 0 == subcom.bmo.run_command("gitlab-runner exec {runner_cmd} {action}")
+    if not command:
+        command = "docker" if find_docker() is not None else "shell"
+    job = job if job else "build"
+    assert 0 == bmo.common.run_command(f"gitlab-runner exec {command} {job}")
 
 if __name__ == "__main__":
     import doctest
