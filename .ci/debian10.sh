778add380e0047c2c536c08acc650a896ff98450
Dilawar Singh <dilawar@subcom.tech>
2022-06-25 13:10:34 +0530

    chore: Adds script to test a virgin debian10.8 image.

---
+++ .ci/debian10.sh
@@ -0,0 +1,7 @@
+#!/usr/bin/env bash
+
+sudo apt-get -y install python3-pip
+python3 -m pip install bmo
+bmo network check-ssl subcom.tech
+bmo run script bootstrap_debian.sh
+bmo run script docker_debian.sh
